26cc5e4f8db6b4590c6651fb43bf3778a0f737db
AaronBriggs <aaron00.briggs@gmail.com>
2020-09-09 17:53:35 -0500

    initalized and generate Actor Model // seeds some data

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  actor = Actor.new({first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title})
+  actor.save
+end
