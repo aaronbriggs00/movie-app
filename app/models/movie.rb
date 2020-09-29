07f0831586e1efd3d395524a5e85ed705a18c8e5
AaronBriggs <aaron00.briggs@gmail.com>
2020-09-28 23:45:03 -0500

    adds genre model and join table to app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,8 @@ class Movie < ApplicationRecord
   validates :plot, length: { in: 6..500 }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   scope :english, -> { where(english: true)}
 end
